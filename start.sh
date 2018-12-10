f6bdbbaac489e0076edae9c0c265fa49fc7e6e2b
Emperorhan <iny27@naver.com>
2018-12-10 11:33:27 +0900

    please

--- start.sh
+++ start.sh
@@ -6,13 +6,15 @@
 #
 ###############################################################################
 
-
 NODEOSBINDIR="/home/eos/eos/build/programs/nodeos"
 DATADIR="$( jq -r '.DIR' "config.conf" )"
+PRODUCER_NAME="$( jq -r '.ACCOUNT' "config.conf" )"
+PUB_KEY="$( jq -r '.PUB_KEY' "config.conf" )"
+PRIV_KEY="$( jq -r '.PRIV_KEY' "config.conf" )"
 
 $DATADIR/stop.sh
 echo -e "Starting Nodeos \n";
 
-$NODEOSBINDIR/nodeos  --delete-all-blocks --genesis-json $DATADIR/genesis.json --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
-#$NODEOSBINDIR/nodeos --replay-blockchain --contracts-console --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid 
-#$NODEOSBINDIR/nodeos  --contracts-console $DATADIR/genesis.json --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
+$NODEOSBINDIR/nodeos -p $PRODUCER_NAME --signature-provider=$PUB_KEY=KEY:$PRIV_KEY --delete-all-blocks --genesis-json $DATADIR/genesis.json --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
+#$NODEOSBINDIR/nodeos -p $PRODUCER_NAME --signature-provider=$PUB_KEY=KEY:$PRIV_KEY --replay-blockchain --contracts-console --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid 
+#$NODEOSBINDIR/nodeos -p $PRODUCER_NAME --signature-provider=$PUB_KEY=KEY:$PRIV_KEY --contracts-console $DATADIR/genesis.json --data-dir $DATADIR --config-dir $DATADIR > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
